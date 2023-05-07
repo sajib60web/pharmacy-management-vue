da67d70729a573b27d01c4eb208f40afbfe5c95f
Sajib Kumar Barai <sajib60wab@gmail.com>
2023-05-07 12:08:31 +0600

    TheSidebar component

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-
 import DashboardPage from "../views/Dashboard.vue";
 import LoginPage from "../views/Login.vue";
 
@@ -9,7 +8,14 @@ const routes = [
     {
         path: "/dashboard",
         component: DashboardPage,
-    }
+        children: [
+        //     { path: "overview", component: Overview },
+        //     { path: "drugs", component: Drugs },
+        //     { path: "vendors", component: Vendors },
+        //     { path: "selling-history", component: SellingHistory },
+        //     { path: "settings", component: Settings },
+        ],
+    },
 ];
 
 const router = createRouter({
