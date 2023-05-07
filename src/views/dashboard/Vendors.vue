e249915c04f31c1e58b9e00add28c30057ab64c4
Sajib Kumar Barai <sajib60wab@gmail.com>
2023-05-07 12:43:50 +0600

    Creating vendors page

--- src/views/dashboard/Vendors.vue
+++ src/views/dashboard/Vendors.vue
@@ -1,7 +1,38 @@
 <template>
-    <div>Vendors</div>
+    <div class="d-flex jc-between ai-center">
+        <h2>All vendors</h2>
+        <the-button @click="addModal = true">Add New</the-button>
+    </div>
+    <table class="mt-4">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Name</td>
+                <td>Description</td>
+                <td>
+                    <img src="/img/edit.png" alt="Edit" class="action-icon" />
+                    <img
+                        src="/img/trash.png"
+                        alt="Delete"
+                        class="action-icon action-icon--delete ml-3"
+                    />
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 <script>
-export default {};
+import TheButton from "../../components/TheButton.vue";
+export default {
+    components: {
+        TheButton,
+    },
+};
 </script>
 <style></style>
